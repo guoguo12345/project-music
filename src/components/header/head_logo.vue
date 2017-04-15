d5ac454529b5ef8e9c2e51394ba207cee35b03c5
guoguo <guoguo@qq.com>
2017-04-15 09:50:34 +0800

    project

---
+++ src/components/header/head_logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo-container">
+    <div class="head-download">
+      <a>爱音乐</a>
+    </div>
+    <div class="head-search" @click="searchRouter">
+      <img src="http://m.kugou.com/v3/static/images/index/search.png">
+    </div>
+  </div>
+</template>
+
+<script type="es6">
+  export default {
+    name: 'head-logo',
+    methods: {
+      searchRouter(){
+        this.$store.commit('showDetailPlayer',false);
+        this.$router.push({path: '/search'});
+      }
+    }
+  }
+</script>
